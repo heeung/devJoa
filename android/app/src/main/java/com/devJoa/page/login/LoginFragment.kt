72e824cb8996f362c2aec986bd2dff2bfb77d299
heeung <skdi6031@naver.com>
2023-06-15 23:49:44 +0900

    heeung : 로그인 텍스트에 따라 버튼 on off 기능 추가

--- android/app/src/main/java/com/devJoa/page/login/LoginFragment.kt
+++ android/app/src/main/java/com/devJoa/page/login/LoginFragment.kt
@@ -4,13 +4,11 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.style.ForegroundColorSpan
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.devJoa.R
@@ -25,6 +23,9 @@ class LoginFragment : Fragment() {
     private val loginViewModel: LoginViewModel by activityViewModels()
 
     lateinit var mActivity: com.devJoa.MainActivity
+
+    private var isLoginTextFilled = false
+    private var isPasswordTextFilled = false
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -46,11 +47,16 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         registerObserver()
+        resisterEditTextChangeListener()
 
-        binding.loginBtn.setOnClickListener {
-            val userId = binding.IDEdit.text.toString()
-            val password = binding.passwordEdit.text.toString()
-            loginViewModel.login(userId, password)
+        binding.loginBtn.apply {
+            isEnabled = false
+            setBackgroundResource(R.drawable.join_radius_button)
+            setOnClickListener {
+                val userId = binding.IDEdit.text.toString()
+                val password = binding.passwordEdit.text.toString()
+                loginViewModel.login(userId, password)
+            }
         }
 
         binding.backBtn.setOnClickListener {
@@ -58,7 +64,7 @@ class LoginFragment : Fragment() {
         }
     }
 
-    fun registerObserver() {
+    private fun registerObserver() {
         loginViewModel.user.observe(viewLifecycleOwner) {
             if (it.userId.isNotEmpty()) { //성공
                 mActivity.showToast("로그인 되었습니다.")
@@ -70,6 +76,61 @@ class LoginFragment : Fragment() {
             } else { //실패
                 mActivity.showToast("id 혹은 password를 확인해주세요.")
             }
+        }
+    }
+
+    private fun resisterEditTextChangeListener() {
+        binding.IDEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(str: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                when(str?.isEmpty()) {
+                     true -> {
+                        isLoginTextFilled = false
+                         changeButtonToggle()
+                    }
+                    else -> {
+                        isLoginTextFilled = true
+                        changeButtonToggle()
+                    }
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        binding.passwordEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(str: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                when(str?.isEmpty()) {
+                    true -> {
+                        isPasswordTextFilled = false
+                        changeButtonToggle()
+                    }
+                    else -> {
+                        isPasswordTextFilled = true
+                        changeButtonToggle()
+                    }
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+    }
+
+    private fun changeButtonToggle() {
+        if (isLoginTextFilled && isPasswordTextFilled) {
+            binding.loginBtn.setBackgroundResource(R.drawable.login_radius_button)
+            binding.loginBtn.isEnabled = true
+        } else {
+            binding.loginBtn.setBackgroundResource(R.drawable.join_radius_button)
+            binding.loginBtn.isEnabled = false
         }
     }
 
